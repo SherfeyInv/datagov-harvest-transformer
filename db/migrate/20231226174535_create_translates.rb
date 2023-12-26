c128dfe8e4fda175396312c0179867ff6e93aa26
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-26 14:00:57 -0700

    lint and format

--- db/migrate/20231226174535_create_translates.rb
+++ db/migrate/20231226174535_create_translates.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateTranslates < ActiveRecord::Migration[6.0]
   def change
     create_table :translates do |t|
--- test/models/translate_test.rb
+++ test/models/translate_test.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class TranslateTest < ActiveSupport::TestCase
-
-  test "convert fgdc to iso19115_3" do
+  test 'convert fgdc to iso19115_3' do
     @translate = translates(:fgdc_to_iso)
-    @file = File.read( File.expand_path("../../fixtures/" + @translate.file, __FILE__) )
+    @file = File.read(File.expand_path("../../fixtures/#{@translate.file}", __FILE__))
     @reader = @translate.reader
     @writer = @translate.writer
 
     @mdReturn = ADIWG::Mdtranslator.translate(
-          file: @file, reader: @reader, writer: @writer)
+      file: @file, reader: @reader, writer: @writer
+    )
 
     assert_not_nil(@mdReturn[:writerOutput])
-
   end
-
 end
