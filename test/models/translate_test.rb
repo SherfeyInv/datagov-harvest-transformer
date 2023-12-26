cf03d9e7860c5f76bd27b6eb62c186acd2389896
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-26 13:33:23 -0700

    add test for mdtranslator

--- test/models/translate_test.rb
+++ test/models/translate_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class TranslateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "convert fgdc to iso19115_3" do
+    @translate = translates(:fgdc_to_iso)
+    @file = File.read( File.expand_path("../../fixtures/" + @translate.file, __FILE__) )
+    @reader = @translate.reader
+    @writer = @translate.writer
+
+    @mdReturn = ADIWG::Mdtranslator.translate(
+          file: @file, reader: @reader, writer: @writer)
+
+    assert_not_nil(@mdReturn[:writerOutput])
+
+  end
+
 end
