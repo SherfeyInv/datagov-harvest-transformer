9b36beca831569ce31eabe49c7ba303115cbff8e
Reid Hewitt <sam.hew.r@gmail.com>
2024-04-23 16:20:12 -0600

    add response tests

--- test/controllers/translates_controller_test.rb
+++ test/controllers/translates_controller_test.rb
@@ -3,11 +3,17 @@
 require 'test_helper'
 
 class TranslatesControllerTest < ActionDispatch::IntegrationTest
-  test 'post transformation of fgdc to iso19115_3' do
-    @translate = translates(:fgdc_to_iso)
-    @file = File.read(File.expand_path("../../fixtures/#{@translate.file}", __FILE__))
-    post '/translates', params: { translate: { file: @file,
-                                               reader: @translate.reader, writer: @translate.writer } }, as: :json
+  test 'post valid transformation of sbJson to mdJson' do
+    @translate = translates(:sbjson_to_mdjson)
+    @file = File.read(File.join(File.dirname(__FILE__), "../fixtures/#{@translate.file}"))
+    post '/translates', params: { file: @file, reader: @translate.reader, writer: @translate.writer }, as: :json
     assert_response 200
+  end
+
+  test 'post invalid transformation of fgdc to iso19115_3' do
+    @translate = translates(:fgdc_to_iso)
+    @file = File.read(File.join(File.dirname(__FILE__), "../fixtures/#{@translate.file}"))
+    post '/translates', params: { file: @file, reader: @translate.reader, writer: @translate.writer }, as: :json
+    assert_response 422
   end
 end
