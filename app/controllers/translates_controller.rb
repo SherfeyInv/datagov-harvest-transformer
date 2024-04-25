7d253b3ec0162b655bea2c98f1a36da7105c250e
Reid Hewitt <sam.hew.r@gmail.com>
2024-04-25 09:37:11 -0600

    format

--- app/controllers/translates_controller.rb
+++ app/controllers/translates_controller.rb
@@ -7,22 +7,33 @@ class TranslatesController < ApplicationController
     reader_name = params[:reader]
     writer_name = params[:writer]
 
-    @md_return = ADIWG::Mdtranslator.translate(
-      file: file_obj, reader: reader_name, writer: writer_name
-    )
+    @md_return =
+      ADIWG::Mdtranslator.translate(
+        file: file_obj,
+        reader: reader_name,
+        writer: writer_name
+      )
 
     @response_info = {}
     @response_info[:success] = true
-    @response_info[:readerStructureStatus] = 'OK'
-    @response_info[:readerStructureMessages] = @md_return[:readerStructureMessages]
-    @response_info[:readerValidationStatus] = 'OK'
-    @response_info[:readerValidationMessages] = @md_return[:readerValidationMessages]
-    @response_info[:readerExecutionStatus] = 'OK'
-    @response_info[:readerExecutionMessages] = @md_return[:readerExecutionMessages]
-    @response_info[:writerStatus] = 'OK'
+    @response_info[:readerStructureStatus] = "OK"
+    @response_info[:readerStructureMessages] = @md_return[
+      :readerStructureMessages
+    ]
+    @response_info[:readerValidationStatus] = "OK"
+    @response_info[:readerValidationMessages] = @md_return[
+      :readerValidationMessages
+    ]
+    @response_info[:readerExecutionStatus] = "OK"
+    @response_info[:readerExecutionMessages] = @md_return[
+      :readerExecutionMessages
+    ]
+    @response_info[:writerStatus] = "OK"
     @response_info[:writerMessages] = @md_return[:writerMessages]
     @response_info[:readerRequested] = @md_return[:readerRequested]
-    @response_info[:readerVersionRequested] = @md_return[:readerVersionRequested]
+    @response_info[:readerVersionRequested] = @md_return[
+      :readerVersionRequested
+    ]
     @response_info[:readerVersionUsed] = @md_return[:readerVersionUsed]
     @response_info[:writerRequested] = @md_return[:writerRequested]
     @response_info[:writerVersion] = @md_return[:writerVersion]
@@ -40,28 +51,28 @@ class TranslatesController < ApplicationController
     a_e_mess = @response_info[:readerExecutionMessages]
     a_w_mess = @response_info[:writerMessages]
 
-    status = 'OK'
-    status = 'NOTICE' if a_s_mess.any? { |s| s.include?('NOTICE') }
-    status = 'WARNING' if a_s_mess.any? { |s| s.include?('WARNING') }
-    status = 'ERROR' if a_s_mess.any? { |s| s.include?('ERROR') }
+    status = "OK"
+    status = "NOTICE" if a_s_mess.any? { |s| s.include?("NOTICE") }
+    status = "WARNING" if a_s_mess.any? { |s| s.include?("WARNING") }
+    status = "ERROR" if a_s_mess.any? { |s| s.include?("ERROR") }
     @response_info[:readerStructureStatus] = status
 
-    status = 'OK'
-    status = 'NOTICE' if a_v_mess.any? { |s| s.include?('NOTICE') }
-    status = 'WARNING' if a_v_mess.any? { |s| s.include?('WARNING') }
-    status = 'ERROR' if a_v_mess.any? { |s| s.include?('ERROR') }
+    status = "OK"
+    status = "NOTICE" if a_v_mess.any? { |s| s.include?("NOTICE") }
+    status = "WARNING" if a_v_mess.any? { |s| s.include?("WARNING") }
+    status = "ERROR" if a_v_mess.any? { |s| s.include?("ERROR") }
     @response_info[:readerValidationStatus] = status
 
-    status = 'OK'
-    status = 'NOTICE' if a_e_mess.any? { |s| s.include?('NOTICE') }
-    status = 'WARNING' if a_e_mess.any? { |s| s.include?('WARNING') }
-    status = 'ERROR' if a_e_mess.any? { |s| s.include?('ERROR') }
+    status = "OK"
+    status = "NOTICE" if a_e_mess.any? { |s| s.include?("NOTICE") }
+    status = "WARNING" if a_e_mess.any? { |s| s.include?("WARNING") }
+    status = "ERROR" if a_e_mess.any? { |s| s.include?("ERROR") }
     @response_info[:readerExecutionStatus] = status
 
-    status = 'OK'
-    status = 'NOTICE' if a_w_mess.any? { |s| s.include?('NOTICE') }
-    status = 'WARNING' if a_w_mess.any? { |s| s.include?('WARNING') }
-    status = 'ERROR' if a_w_mess.any? { |s| s.include?('ERROR') }
+    status = "OK"
+    status = "NOTICE" if a_w_mess.any? { |s| s.include?("NOTICE") }
+    status = "WARNING" if a_w_mess.any? { |s| s.include?("WARNING") }
+    status = "ERROR" if a_w_mess.any? { |s| s.include?("ERROR") }
     @response_info[:writerStatus] = status
 
     # check for errors returned by parser, validator, reader, and writer
--- test/controllers/translates_controller_test.rb
+++ test/controllers/translates_controller_test.rb
@@ -1,19 +1,37 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 class TranslatesControllerTest < ActionDispatch::IntegrationTest
-  test 'post valid transformation of sbJson to mdJson' do
+  test "post valid transformation of sbJson to mdJson" do
     @translate = translates(:sbjson_to_mdjson)
-    @file = File.read(File.join(File.dirname(__FILE__), "../fixtures/#{@translate.file}"))
-    post '/translates', params: { file: @file, reader: @translate.reader, writer: @translate.writer }, as: :json
+    @file =
+      File.read(
+        File.join(File.dirname(__FILE__), "../fixtures/#{@translate.file}")
+      )
+    post "/translates",
+         params: {
+           file: @file,
+           reader: @translate.reader,
+           writer: @translate.writer
+         },
+         as: :json
     assert_response 200
   end
 
-  test 'post invalid transformation of fgdc to iso19115_3' do
+  test "post invalid transformation of fgdc to iso19115_3" do
     @translate = translates(:fgdc_to_iso)
-    @file = File.read(File.join(File.dirname(__FILE__), "../fixtures/#{@translate.file}"))
-    post '/translates', params: { file: @file, reader: @translate.reader, writer: @translate.writer }, as: :json
+    @file =
+      File.read(
+        File.join(File.dirname(__FILE__), "../fixtures/#{@translate.file}")
+      )
+    post "/translates",
+         params: {
+           file: @file,
+           reader: @translate.reader,
+           writer: @translate.writer
+         },
+         as: :json
     assert_response 422
   end
 end
